4981392ef4efc9f57a60cf41910092378d7950de
Stan <stan@routineforbots.com>
2025-05-22 13:09:29 +0200

    Started Let's Get Rusty course

---
+++ src/bin/guessing_game.rs
@@ -0,0 +1,11 @@
+use std::io;
+
+fn main() {
+    println!("Guess the number");
+
+    println!("Enter yours: ");
+
+    let mut guess = String::new(); // we need a variable to be mutable 
+
+    io::stdnin().read_line
+
